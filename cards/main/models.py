8628b2117a5cff194af7070c4da46597949292a0
georg <g.kuhticki@gmail.com>
2021-08-08 19:57:27 +0300

    V 0.8.2.5 qr code  не принимает значение id  filefield принял текстовый документ.

--- cards/main/models.py
+++ cards/main/models.py
@@ -3,6 +3,7 @@ from django.core.files import File
 from PIL import Image, ImageDraw
 import qrcode
 from io import BytesIO
+from django.core.files.base import ContentFile, File
 # Create your models here.
 class User(models.Model):
     POWER = [
@@ -73,117 +74,6 @@ class User(models.Model):
     b1_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
     b1_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
 
-    b2_power = models.CharField(("Включение кнопки 2"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b2_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b2_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b2_title = models.CharField(("Заголовок"), max_length=255, )
-    b2_b_text = models.CharField(("Текст"), max_length=255, )
-    b2_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b2_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b3_power = models.CharField(("Включение кнопки 3"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b3_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b3_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b3_title = models.CharField(("Заголовок"), max_length=255, )
-    b3_b_text = models.CharField(("Текст"), max_length=255, )
-    b3_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b3_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b4_power = models.CharField(("Включение кнопки 4"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b4_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b4_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b4_title = models.CharField(("Заголовок"), max_length=255, )
-    b4_b_text = models.CharField(("Текст"), max_length=255, )
-    b4_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b4_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b5_power = models.CharField(("Включение кнопки 5"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b5_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b5_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b5_title = models.CharField(("Заголовок"), max_length=255, )
-    b5_b_text = models.CharField(("Текст"), max_length=255, )
-    b5_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b5_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b6_power = models.CharField(("Включение кнопки 6"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b6_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b6_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b6_title = models.CharField(("Заголовок"), max_length=255, )
-    b6_b_text = models.CharField(("Текст"), max_length=255, )
-    b6_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b6_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b7_power = models.CharField(("Включение кнопки 7"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b7_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b7_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b7_title = models.CharField(("Заголовок"), max_length=255, )
-    b7_b_text = models.CharField(("Текст"), max_length=255, )
-    b7_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b7_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b8_power = models.CharField(("Включение кнопки 8"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b8_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b8_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b8_title = models.CharField(("Заголовок"), max_length=255, )
-    b8_b_text = models.CharField(("Текст"), max_length=255, )
-    b8_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b8_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b9_power = models.CharField(("Включение кнопки 9"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b9_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b9_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b9_title = models.CharField(("Заголовок"), max_length=255, )
-    b9_b_text = models.CharField(("Текст"), max_length=255, )
-    b9_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b9_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b10_power = models.CharField(("Включение кнопки 10"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b10_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b10_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b10_title = models.CharField(("Заголовок"), max_length=255, )
-    b10_b_text = models.CharField(("Текст"), max_length=255, )
-    b10_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b10_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b11_power = models.CharField(("Включение кнопки 11"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b11_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b11_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b11_title = models.CharField(("Заголовок"), max_length=255, )
-    b11_b_text = models.CharField(("Текст"), max_length=255, )
-    b11_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b11_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b12_power = models.CharField(("Включение кнопки 12"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b12_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b12_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b12_title = models.CharField(("Заголовок"), max_length=255, )
-    b12_b_text = models.CharField(("Текст"), max_length=255, )
-    b12_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b12_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b13_power = models.CharField(("Включение кнопки 13"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b13_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b13_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b13_title = models.CharField(("Заголовок"), max_length=255, )
-    b13_b_text = models.CharField(("Текст"), max_length=255, )
-    b13_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b13_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b14_power = models.CharField(("Включение кнопки 14"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b14_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b14_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b14_title = models.CharField(("Заголовок"), max_length=255, )
-    b14_b_text = models.CharField(("Текст"), max_length=255, )
-    b14_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b14_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
-
-    b15_power = models.CharField(("Включение кнопки 15"), choices=POWER, max_length=255, blank=False, default="display: none;")
-    b15_color = models.CharField(("Цвет кнопки"), choices=COLOR, max_length=255, blank=False, default="background-color: rgba(0, 67, 78, 1);")
-    b15_icons = models.CharField(("Иконка"), choices=ICONS, max_length=255, blank=False, default="tel")
-    b15_title = models.CharField(("Заголовок"), max_length=255, )
-    b15_b_text = models.CharField(("Текст"), max_length=255, )
-    b15_api_cho = models.CharField(("Функционал кнопки"), choices=API_CHO, max_length=355, blank=False, default="tel:+7")
-    b15_api_inp = models.CharField(verbose_name="Ваши данные", max_length=255, default="8 800 200 06 07")
 
     def __str__(self):
         return self.name
@@ -192,58 +82,25 @@ class User(models.Model):
         verbose_name="страничку пользователя"
         verbose_name_plural = "Пользователи"
 
-    # def Test(self):
-    #     id = self.id
-    #     strid = str(id)
-    #
-    #     url = "http://127.0.0.1:8000/user/"
-    #     furl = url + strid
-    #     img = qrcode.make(furl)
-    #     img.save('myqrcode.png')
-    #
-    #
-    #     a = 500
-    #     aa = str(a)
-    #     cc = strid + aa
-    #     id1 = cc
-    #
-    #     return id1
 
     def save(self, *args, **kwargs):
-        id = self.id
-        strid = str(id)
+        # id = self.id
+        # strid = str(id)
+        #
+        # url = "http://127.0.0.1:8000/user/"
+        # furl = url + strid
+        # qrcode_img = qrcode.make(furl)
+        # canvas = Image.new('RGB', (390, 390), 'white')
+        # draw = ImageDraw.Draw(canvas)
+        # canvas.paste(qrcode_img)
+        # fname = f'qr_code-{self.id}.png'
+        # buffer = BytesIO()
+        # canvas.save(buffer, 'PNG')
+        # self.qr_code.save(fname, File(buffer), save=False)
+        # canvas.close()
 
-        url = "http://127.0.0.1:8000/user/"
-        furl = url + strid
-        qrcode_img = qrcode.make(furl)
-        canvas = Image.new('RGB', (390, 390), 'white')
-        draw = ImageDraw.Draw(canvas)
-        canvas.paste(qrcode_img)
-        fname = f'qr_code-{self.id}.png'
-        buffer = BytesIO()
-        canvas.save(buffer, 'PNG')
-        self.qr_code.save(fname, File(buffer), save=False)
-        canvas.close()
+        myfile = ContentFile("hello world")
+        new_name = "new_name.txt"
+        self.file.save(new_name, File(myfile), save=False)
+
         super().save(*args, **kwargs)
-
-
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    birth_date = models.DateField()
-
-    def baby_boomer_status(self):
-        "Returns the person's baby-boomer status."
-        import datetime
-        if self.birth_date < datetime.date(1945, 8, 1):
-            return "Pre-boomer"
-        elif self.birth_date < datetime.date(1965, 1, 1):
-            return "Baby boomer"
-        else:
-            return "Post-boomer"
-
-    @property
-    def full_name(self):
-        "Returns the person's full name."
-        return '%s %s' % (self.first_name, self.last_name)
